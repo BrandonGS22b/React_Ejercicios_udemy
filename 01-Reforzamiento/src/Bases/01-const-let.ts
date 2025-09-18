d56069b1fb6f59fc12c929b5f6f1bddbdde2f53b
Brandon_Garcia <brandondulian@hotmail.com>
2025-09-18 10:14:58 -0500

    Reforzamiento de Variables

---
+++ 01-Reforzamiento/src/Bases/01-const-let.ts
@@ -0,0 +1,14 @@
+const firstName :String = 'brandon';
+const lastName = 'Garcia';
+let diceNumber = 5;
+
+const containsLetterC=lastName.includes('c');
+
+
+
+
+
+
+console.log('Mi nombre es:'+firstName+lastName );
+//usar esta de ahora en adelante nos va mostrar el tipo de dato
+console.log({containsLetterC,diceNumber,firstName })
--- 01-Reforzamiento/src/main.ts
+++ 01-Reforzamiento/src/main.ts
@@ -1,11 +1,13 @@
 import './style.css'
-
+import './Bases/01-const-let'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
   <h1>  hola mundo </h1>
 
   </div>
+  
 `
+
 
 
